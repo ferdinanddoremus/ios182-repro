d4715b911fb9513c512129d9976e902c8040c883
Grorémus <ferdinanddoremus@gmail.com>
2024-12-13 10:20:45 +0100

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
-module.exports = function (api) {
-  api.cache(true)
-
+module.exports = (api) => {
+  api.cache(true);
   return {
-    // other config
-    plugins: [
-        // other plugins
-        ['react-native-unistyles/plugin']
-    ]
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-unistyles/plugin'],
   }
 }
